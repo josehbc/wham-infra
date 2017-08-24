ae88ddb889652964e7a2d0242122dd22509aaa44
gambtho <tgamble@thougthworks.com>
2017-08-24 12:58:19 -0400

    added bastion setup

---
+++ terraform/bastion/main.tf
@@ -0,0 +1,27 @@
+variable "region"              { default = "us-east-1" }
+
+module "bastion" {
+  source                       = "github.com/saksdirect/aws-ref-arch/terraform/bastion"
+
+  availability_zones = [
+    "${data.aws_availability_zones.available.names[0]}",
+    "${data.aws_availability_zones.available.names[1]}"
+  ]
+  bastion_ami = "ami-a4c7edb2"
+  bastion_subnet_ids           = ["subnet-7beca033", "subnet-809e0aac"]
+  bastion_security_groups      = ["sg-df68f9af"]
+}
+
+data "aws_availability_zones" "available" {}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform.wham"
+    key    = "state/bastion/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "${var.region}"
+}
